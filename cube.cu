f67dbb20e34588e1794d35498271718def52f9fd
Saksham Dhuria <107601836+sakshamdhuria@users.noreply.github.com>
2024-01-29 19:11:10 -0600

    Second update for #39

--- cube.cu
+++ cube.cu
@@ -3,7 +3,7 @@
 __global__ void cube (float * d_out, float * d_in){
     int idx = threadIdx.x;
     float f = d_in[idx];
-    d_out[idx] = f*f*f;
+    d_out[idx] = f*f*f*f;
 }
 
 int main(int argc, char ** argv){
